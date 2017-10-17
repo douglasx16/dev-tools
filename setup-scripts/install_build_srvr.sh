d2d79d1adad860146edc30982cf97a7a7618793d
Douglas Rodrigues <drodrigues@uoldiveo.com>
2017-10-17 15:47:05 -0200

    Change repo to download build

--- setup-scripts/install_build_srvr.sh
+++ setup-scripts/install_build_srvr.sh
@@ -2,7 +2,7 @@
 export BUILD_BASE='/home/oneops/build'
 export OO_HOME='/home/oneops'
 export SEARCH_SITE=localhost
-export GITHUB_URL='https://github.com/oneops'
+export GITHUB_URL='https://github.com/douglasx16'
 echo "$BUILD_BASE"
 
 mkdir -p "$BUILD_BASE"
